ad310dfd0e431295d0e3e02234671d6202c6067b
savas <savasaydin@gmail.com>
2013-06-18 23:16:51 +0200

    generates species and starts game

--- abstract_factory/src/species.erl
+++ abstract_factory/src/species.erl
@@ -1,7 +1,25 @@
 -module(species).
 
--export([generate_race/1]).
+-export([start/1
+	]).
+
+-include("starcraft.hrl").
+
+start(Race) when Race == terran orelse
+		 Race == zerg orelse
+		 Race == protoss ->
+    Species = generate_race(Race),
+    start_game(Species);
+start(_) ->
+    io:format("Not valid species").
+
+start_game(Species) ->
+    io:format("Generating ~p race~n"
+	      "with 3 ~p "
+	      "and a ~p~n",
+	      [Species#species.race,
+	      Species#species.worker_unit,
+	      Species#species.base_building]).
 
 generate_race(Race) ->
     Race:generate_species().
-
