0c19098d3ad9abca7af3ea6d0270d2dd0bd0e2bb
savas <savasaydin@gmail.com>
2013-06-18 23:24:27 +0200

    renamed species.erl module to starcraft.erl

---
+++ abstract_factory/test/starcraft_tests.erl
@@ -0,0 +1,59 @@
+-module(starcraft_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("starcraft.hrl").
+
+all_feature_test_() ->
+    {foreach,
+     fun setup/0,
+     fun cleanup/1,
+     [
+      fun terran_species/1,
+      fun zerg_species/1,
+      fun protoss_species/1,
+      fun undefined_species/1
+     ]}.
+      
+terran_species(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    starcraft:start(terran),
+	    ExpectedResult = <<"Generating terran race\nwith 3 scv and a command_center\n">>,
+	    assert_feature(ExpectedResult, stdout)
+    end.
+
+zerg_species(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    starcraft:start(zerg),
+	    ExpectedResult = <<"Generating zerg race\nwith 3 drone and a hatchery\n">>,
+	    assert_feature(ExpectedResult, stdout)
+    end.
+
+protoss_species(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    starcraft:start(protoss),
+	    ExpectedResult = <<"Generating protoss race\nwith 3 probe and a nexus\n">>,
+	    assert_feature(ExpectedResult, stdout)
+    end.
+
+undefined_species(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    starcraft:start(undefined),
+	    ExpectedResult = <<"Not valid species">>,
+	    assert_feature(ExpectedResult, stdout)
+    end.
+
+assert_feature(ExpectedResult, File) ->
+    ?assertEqual({ok, ExpectedResult}, file:read_file(File)).
+
+setup() ->
+    {ok, F} = file:open(stdout, [write]),
+    F.
+
+cleanup(F) ->
+    file:close(F),
+    file:delete(stdout).
